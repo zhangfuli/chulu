cce0529702371f05162d1635df619c59df7ba933
zhangfuli <1171067930@qq.com>
2018-03-30 12:10:32 +0800

    guide 上下 滑动

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -8,6 +8,7 @@ Page({
     startX: '',
     startY: '',
     items: [], //用于记录路径上每个viewport的位置 
+    originItems:[],
     /**
      * 以及class信息
      * [{
@@ -79,6 +80,7 @@ Page({
     let self = this;
     query.selectAll('.block').boundingClientRect().exec(function (rects) {
       self.items = rects[0];
+      self.originItems = rects[0];
       for(let i=0; i<self.items.length; i++){
         if(i === 0 || i === self.items.length -1){
           self.items[i].class = {
@@ -87,29 +89,34 @@ Page({
           }   
         }else{
           self.items[i].class = {
-            parentClass:"",
-            childClass:['', '', ''] //三个子view
+            parentClass:"start-end",
+            childClass:['hide', 'hide', 'hide'] //三个子view
           }
         }  
-        self.setData({items: self.items});  
+       
       }
-  
-  
+      self.setData({ items: self.items });  
+      self.originItems = JSON.parse(JSON.stringify(self.items));
     })
   },
   /**
    * 用户开始滑动
    */
   handletouchmove: function (event) {
+    this.items = JSON.parse(JSON.stringify(this.originItems))
+    this.setData({items: this.originItems})
     this.startX = event.touches[0].clientX;
     this.startY = event.touches[0].clientY;
     this.currentId = event.currentTarget.id;
+    this.startId = event.currentTarget.id;
+
     //判定从起点开始滑动
     if( event.currentTarget.dataset.isstart === 'false'){
       wx.showToast({
         title: '请选择起点',
         image: '../images/error.png',
       })
+      this.setData({ items: this.originItems })
     }
   },
   /**
@@ -122,30 +129,53 @@ Page({
       if ((this.items[i].left - this.startX < 0 && this.items[i].right - this.startX > 0)
         && (this.items[i].top - this.startY < 0 && this.items[i].bottom - this.startY > 0)) {
         event.currentTarget.id = this.items[i].id;
+        this.items[i].dataset.touched = "true"
       }
     }
-  
     //当用户的手从一个view滑动到另一个view
     if (event.currentTarget.id !== this.currentId){
       this.addPath(this.currentId, event.currentTarget.id);
       this.currentId = event.currentTarget.id 
     }  
   },
+  touchend: function(){
+    let j=0;
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].dataset.touched == "true"){
+        j++;
+      }
+    }
+    if (this.items.length == j){
+      console.log("qunaguo")
+      wx.showToast({
+        title: 'great！清除缓存',
+        icon: 'success',
+        duration: 2000
+      })
+      this.setData({ items: this.originItems })
+      this.items = JSON.parse(JSON.stringify(this.originItems))
+      //清除数据缓存
+    }else{
+      wx.showToast({
+        title: '没有全遍历哦',
+        image: '../images/error.png',
+        duration: 2000
+      })
+      this.setData({ items: this.originItems })
+      this.items = JSON.parse(JSON.stringify(this.originItems))
+    }
+  },
   addPath: function(preId, nextId){
     //手指滑出， 刷新页面
     if(nextId === this.startId){
       //刷新--重新导航到当前页面
       wx.showToast({
-        title: '失败',
+        title: '滑出去啦',
         image: '../images/error.png',
-        complete: function () {
-          setTimeout(function(){
-            wx.navigateTo({
-              url: '/pages/guide/guide'
-            })
-          },1500)    
-        }
+        duration: 2000
       })
+      this.setData({items: this.originItems})
+      this.items = JSON.parse(JSON.stringify(this.originItems))
     }
     
     /**
@@ -163,41 +193,69 @@ Page({
     //  * '4': 'not move'
     //  *};
     //  * */
+    let self = this;
     switch (direction) {
       case 'down':
-        for (let i in this.items) {
-          if (this.items[i].id === preId) {
-            switch(preId){
+        for (let i in self.items) { 
+          if (self.items[i].id === preId) {//正则不好匹配
+            switch (self.items[i].class.parentClass){
               case 'start-end':
+                self.items[i].class.parentClass = 'block-down-path';
+                self.items[i].class.childClass = ['path up', 'endpoint', 'path down hide'];
+                break;
               case 'block-up-path':
+                self.items[i].class.parentClass = 'block-up-down-path';
+                self.items[i].class.childClass = ['path up', 'endpoint', 'path down'];
+                break;
               case 'block-left-path':
+                self.items[i].class.parentClass = 'block-left-down-path';
+                self.items[i].class.childClass = ['row-up-path', 'row-path', 'row-endpoint'];
+                break;
               case 'block-right-path':
+                self.items[i].class.parentClass = 'block-right-down-path';
+                self.items[i].class.childClass = ['row-up-path', 'row-path', 'row-endpoint'];
+                break;
             }
-
           }
           if (this.items[i].id === nextId) {
-            console.log(nextId)
+            self.items[i].class.parentClass = 'block-up-path';
+            self.items[i].class.childClass = ['path up', 'endpoint', 'path down hide'];
           }
         }
         break;
-      // case 数字:
-      //   语句;
-      //   break;
+      case "up":
+        for (let i in self.items) {
+          if (self.items[i].id === preId) {
+            switch (self.items[i].class.parentClass) {
+              case 'start-end':
+                self.items[i].class.parentClass = 'block-up-path hide';
+                self.items[i].class.childClass = ['path up', 'endpoint', 'path down hide'];
+                break;
+              case 'block-down-path':
+                self.items[i].class.parentClass = 'block-up-down-path';
+                self.items[i].class.childClass = ['path up', 'endpoint', 'path down'];
+                break;
+              case 'block-left-path':
+                self.items[i].class.parentClass = 'block-up-left-path';
+                self.items[i].class.childClass = ['row-up-path', 'row-path', 'row-endpoint'];
+                break;
+              case 'block-right-path':
+                self.items[i].class.parentClass = 'block-up-right-path';
+                self.items[i].class.childClass = ['row-up-path', 'row-path', 'row-endpoint'];
+            }
+          }
+          if (this.items[i].id === nextId) {
+            self.items[i].class.parentClass = 'block-down-path';
+            self.items[i].class.childClass = ['path up', 'endpoint', 'path down hide'];
+          }
+        }
+        break;
       // case 字符串:
       //   语句;
       // default:
       //   语句;
     }
-    // if (direction == 'down'){
-    //   for(let i in this.items){
-    //     if(this.items[i].id === preId){
-    //       console.log(this.items[i].class)
-    //     }
-    //     if(this.items[i].id === nextId){
-    //       console.log(nextId)
-    //     }
-    //   }
-    // }
+    self.setData({items:self.items})
   },
 
   //得到左上角坐标
